71440bd25aa233431a94cb179c753217c4cd4e68
liam-strand <strandliam@gmail.com>
2024-10-25 17:45:39 -0500

    any color you like...

--- lossy_socket.py
+++ lossy_socket.py
@@ -9,8 +9,13 @@ random.seed(398120)
 
 
 class SimulationParams:
-    def __init__(self, loss_rate: float=0.0, corruption_rate: float=0.0,
-                 max_delivery_delay: float=0.0, become_reliable_after: float=100000.0):
+    def __init__(
+        self,
+        loss_rate: float = 0.0,
+        corruption_rate: float = 0.0,
+        max_delivery_delay: float = 0.0,
+        become_reliable_after: float = 100000.0,
+    ):
         self.start_time = time()
         self.loss_rate = loss_rate
         self.corruption_rate = corruption_rate
@@ -19,6 +24,7 @@ class SimulationParams:
 
     def forced_reliable(self) -> bool:
         return time() - self.start_time > self.become_reliable_after
+
 
 class SimulationStats:
     def __init__(self):
@@ -32,10 +38,16 @@ class SimulationStats:
         # print some stats
         print("PACKETS_SENT=%d" % self.packets_sent)
         print("UDP_BYTES_SENT=%d" % self.bytes_sent)
-        print("ETH_BYTES_SENT=%d" % (self.bytes_sent + (18+20+8) * self.packets_sent))
+        print(
+            "ETH_BYTES_SENT=%d"
+            % (self.bytes_sent + (18 + 20 + 8) * self.packets_sent)
+        )
         print("PACKETS_RECV=%d" % self.packets_recv)
         print("UDP_BYTES_RECV=%d" % self.bytes_recv)
-        print("ETH_BYTES_RECV=%d" % (self.bytes_recv + (18+20+8) * self.packets_recv))
+        print(
+            "ETH_BYTES_RECV=%d"
+            % (self.bytes_recv + (18 + 20 + 8) * self.packets_recv)
+        )
 
 
 # global simulation parameters
@@ -47,22 +59,29 @@ class LossyUDP(socket):
     def __init__(self):
         self.stopped = False
         super().__init__(AF_INET, SOCK_DGRAM)
-        # make calls to socket.recvfrom timeout after one second, so that self.stopped is checked
+        # make calls to socket.recvfrom timeout after one second, so that
+        # self.stopped is checked
         self.settimeout(1)
 
     def __del__(self):
-        # for our purposes, we always want to unbind the port when the app stops
+        # for our purposes, we always want to unbind the port when the app
+        # stops
         super().close()
 
     def sendto(self, message: bytes, dst: Tuple[str, int]):
         """Unlike the sendto method provided by the BSD socket lib,
-           this method never blocks (because it schedules the transmission on a thread)."""
+        this method never blocks (because it schedules the transmission on a
+        thread).
+        """
         if len(message) > 1472:
-            raise RuntimeError("You are trying to send more than 1472 bytes in one UDP packet!")
+            raise RuntimeError(
+                "You are trying to send more than 1472 bytes in one UDP packet!"
+            )
         with stats.lock:
             stats.packets_sent += 1
             stats.bytes_sent += len(message)
-        # sleep() spaces out the requests enough to eliminate reordering caused by the OS process/thread scheduler.
+        # sleep() spaces out the requests enough to eliminate reordering caused
+        # by the OS process/thread scheduler.
         # It also limits the peak throughput of the socket :(
         sleep(0.01)
         if random.random() < sim.loss_rate and not sim.forced_reliable():
@@ -73,27 +92,43 @@ class LossyUDP(socket):
                 # flip an arbitrary number of bits in the packet:
                 bits_flipped = 0
                 for bit_to_flip in range(len(message) * 8):
-                    # probability of corrupting the packet in at least one bit is ~ sim.corruption_rate
-                    if random.random() < sim.corruption_rate / (len(message) * 8):
+                    # probability of corrupting the packet in at least one bit
+                    # is ~ sim.corruption_rate
+                    if random.random() < sim.corruption_rate / (
+                        len(message) * 8
+                    ):
                         bits_flipped += 1
                         # corrupt the packet
                         byte_to_be_flipped = message[int(bit_to_flip / 8)]
-                        flipped_byte = byte_to_be_flipped ^ (1 << (bit_to_flip % 8))
+                        flipped_byte = byte_to_be_flipped ^ (
+                            1 << (bit_to_flip % 8)
+                        )
                         # bytes type is not mutable, but bytearray is:
                         msg_array = bytearray(message)
                         msg_array[int(bit_to_flip / 8)] = flipped_byte
                         message = bytes(msg_array)
-                        print("outgoing UDP packet's bit number %d was flipped by the simulator."
-                              % bit_to_flip)
+                        print(
+                            "outgoing UDP packet's bit number %d was flipped by the simulator."
+                            % bit_to_flip
+                        )
                 if bits_flipped > 0:
-                    print("total of %d bits flipped in the packet" % bits_flipped)
-            # send message after a random delay.  The randomness will reorder packets
-            Timer(0 if sim.forced_reliable() else random.random() * sim.max_delivery_delay,
-                  lambda: super(self.__class__, self).sendto(message, dst)).start()
+                    print(
+                        "total of %d bits flipped in the packet" % bits_flipped
+                    )
+            # send message after a random delay.  The randomness will reorder
+            # packets
+            Timer(
+                (
+                    0
+                    if sim.forced_reliable()
+                    else random.random() * sim.max_delivery_delay
+                ),
+                lambda: super(self.__class__, self).sendto(message, dst),
+            ).start()
 
-    def recvfrom(self, bufsize: int=2048) -> (bytes, (str, int)):
+    def recvfrom(self, bufsize: int = 2048) -> (bytes, (str, int)):
         """Blocks until a packet is received or self.stoprecv() is called.
-           returns (data, (source_ip, source_port))"""
+        returns (data, (source_ip, source_port))"""
         while not self.stopped:
             try:
                 # wait for a packet, but timeout after one second
@@ -109,7 +144,7 @@ class LossyUDP(socket):
                 continue
             else:
                 return data, addr
-        return b'', ("", 0)
+        return b"", ("", 0)
 
     def stoprecv(self) -> None:
         self.stopped = True
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from streamer import Streamer
 import sys
 import lossy_socket
 
-NUMS=1000
+NUMS = 1000
 
 
 def receive(s):
@@ -10,8 +10,8 @@ def receive(s):
     str_buf = ""
     while expected < NUMS:
         data = s.recv()
-        print("recv returned {%s}" % data.decode('utf-8'))
-        str_buf += data.decode('utf-8')
+        print("recv returned {%s}" % data.decode("utf-8"))
+        str_buf += data.decode("utf-8")
         for t in str_buf.split(" "):
             if len(t) == 0:
                 # there could be a "" at the start or the end, if a space is there
@@ -19,9 +19,9 @@ def receive(s):
             if int(t) == expected:
                 print("got %d!" % expected)
                 expected += 1
-                str_buf = ''
+                str_buf = ""
             elif int(t) > expected:
-                print("ERROR: got %s but was expecting %d" %(t, expected))
+                print("ERROR: got %s but was expecting %d" % (t, expected))
                 sys.exit(-1)
             else:
                 # we only received the first part of the number at the end
@@ -29,47 +29,60 @@ def receive(s):
                 str_buf = t
                 break
 
-    
+
 def host1(listen_port, remote_port):
-    s = Streamer(dst_ip="localhost", dst_port=remote_port,
-                 src_ip="localhost", src_port=listen_port)
+    s = Streamer(
+        dst_ip="localhost",
+        dst_port=remote_port,
+        src_ip="localhost",
+        src_port=listen_port,
+    )
     receive(s)
     print("STAGE 1 TEST PASSED!")
     # send large chunks of data
     i = 0
     buf = ""
     while i < NUMS:
-        buf += ("%d " % i)
-        if len(buf) > 12345 or i == NUMS-1:
+        buf += "%d " % i
+        if len(buf) > 12345 or i == NUMS - 1:
             print("sending {%s}" % buf)
-            s.send(buf.encode('utf-8'))
+            s.send(buf.encode("utf-8"))
             buf = ""
         i += 1
     s.close()
     print("CHECK THE OTHER SCRIPT FOR STAGE 2 RESULTS.")
 
-        
+
 def host2(listen_port, remote_port):
-    s = Streamer(dst_ip="localhost", dst_port=remote_port,
-                 src_ip="localhost", src_port=listen_port)
+    s = Streamer(
+        dst_ip="localhost",
+        dst_port=remote_port,
+        src_ip="localhost",
+        src_port=listen_port,
+    )
     # send small pieces of data
     for i in range(NUMS):
-        buf = ("%d " % i)
+        buf = "%d " % i
         print("sending {%s}" % buf)
-        s.send(buf.encode('utf-8'))
+        s.send(buf.encode("utf-8"))
     receive(s)
     s.close()
     print("STAGE 2 TEST PASSED!")
 
 
 def main():
-    lossy_socket.sim = lossy_socket.SimulationParams(loss_rate=0.0, corruption_rate=0.0,
-                                                     max_delivery_delay=0.0,
-                                                     become_reliable_after=100000.0)
+    lossy_socket.sim = lossy_socket.SimulationParams(
+        loss_rate=0.0,
+        corruption_rate=0.0,
+        max_delivery_delay=0.0,
+        become_reliable_after=100000.0,
+    )
 
     if len(sys.argv) < 4:
         print("usage is: python3 test.py [port1] [port2] [1|2]")
-        print("First run with last argument set to 1, then with 2 (in two different terminals on the same machine")
+        print(
+            "First run with last argument set to 1, then with 2 (in two different terminals on the same machine"
+        )
         sys.exit(-1)
     port1 = int(sys.argv[1])
     port2 = int(sys.argv[2])
